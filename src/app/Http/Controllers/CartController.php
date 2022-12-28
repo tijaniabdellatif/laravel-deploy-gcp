e503857c4e3a7c76adbde9602bc08515fea9481d
arryoub <gorychama@gmail.com>
2022-12-28 13:48:09 +0100

    build

--- src/app/Http/Controllers/CartController.php
+++ src/app/Http/Controllers/CartController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cart;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
     public function index(){
 
-          return view("index");
+
+          $cart = Cart::find(1);
+
+          return view("index",compact('cart'));
     }
 }
